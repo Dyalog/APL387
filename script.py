09d14f0ad481ccb9a1e6d8599ebd6daa4391fcaf
RubenVerg <RubenVerg@outlook.com>
2025-07-10 12:22:16 +0200

    Embed commit as a version string (fixes #27)

--- script.py
+++ script.py
@@ -14,6 +14,12 @@ path = sys.argv[1]
 
 apl387 = fontforge.open(f'{path}/APL387.ufo2')
 
+print(apl387.version)
+
+apl387.version = os.getenv('COMMIT') or 'unknown'
+
+print(apl387.version)
+
 try:
 	os.mkdir(f'{path}/output')
 except:
