3b9de2a7f7ce373ea9a46e97e38a0d2e9212eb26
RubenVerg <RubenVerg@outlook.com>
2026-06-03 11:32:21 +0200

    Index and compare pages for APL335

--- features.js
+++ features.js
@@ -1,11 +1,12 @@
 const withBakedDownload = document.querySelector('#wb');
+const base = document.querySelector('[data-base]')?.dataset?.base ?? 'APL387';
 
 const styles = new Map([...document.querySelectorAll('[type=checkbox]')].map(e => [e.value, e]));
 for (const element of styles.values()) {
 	element.addEventListener('input', () => {
 		const settings = [...styles.entries()].flatMap(([s, e]) => e.checked ? ['"' + s + '"'] : []).join(', ');
 		document.body.style.fontFeatureSettings = settings.trim().length ? settings : 'unset';
-		withBakedDownload.href = `APL387-${settings.replaceAll(', ', '-').replaceAll('"', '')}.ttf`;
+		withBakedDownload.href = `${base}-${settings.replaceAll(', ', '-').replaceAll('"', '')}.ttf`;
 		if (settings.trim().length) withBakedDownload.classList.add('s');
 		else withBakedDownload.classList.remove('s');
 	});
